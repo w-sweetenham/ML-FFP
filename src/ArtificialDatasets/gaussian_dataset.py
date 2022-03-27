aa2a102bbba07b4a47785586af2f15e0d7fe0e6f
William Sweetenham <william@sweetenham.org>
2022-03-27 12:16:17 +0100

    Updated GaussianDataset class so each class si in general formed of multiple gaussians

--- src/ArtificialDatasets/gaussian_dataset.py
+++ src/ArtificialDatasets/gaussian_dataset.py
@@ -1,19 +1,22 @@
 import numpy as np
 
 class GaussianDataset:
-    def __init__(self, cluster_specs, num_samples, relative_probs=None):
+    def __init__(self, class_specs, num_samples, relative_probs=None):
         if relative_probs is None:
-            self.relative_probs = [1/len(cluster_specs)]*len(cluster_specs)
+            self.relative_probs = [1/len(class_specs)]*len(class_specs)
         else:
             self.relative_probs = relative_probs
         self.num_datapoints = num_samples
-        self.cluster_specs = cluster_specs
-        cluster_indexes = np.random.choice(np.arange(len(cluster_specs)), num_samples, self.relative_probs)
+        self.class_specs = class_specs
+        class_indexes = np.random.choice(np.arange(len(class_specs)), num_samples, self.relative_probs)
         self.datapoints = []
-        for n in range(len(self.cluster_specs)):
-            num_samples = sum(cluster_indexes == n)
-            samples = np.random.multivariate_normal(self.cluster_specs[n][0], self.cluster_specs[n][1], num_samples)
-            indexes = np.array([[n]*num_samples])
-            self.datapoints.append(np.concatenate([indexes.T, samples], axis=1))
+        for n in range(len(self.class_specs)):
+            num_samples_for_class = sum(class_indexes == n)
+            gaussian_indexes = np.random.choice(np.arange(len(self.class_specs[n])), num_samples_for_class)
+            for i in range(len(self.class_specs[n])):
+                num_samples_for_gaussian = sum(gaussian_indexes == i)
+                gaussian_datapoints = np.random.multivariate_normal(self.class_specs[n][i][0], self.class_specs[n][i][1], num_samples_for_gaussian)
+                indexes = np.array([[n]*num_samples_for_gaussian])
+                self.datapoints.append(np.concatenate([indexes.T, gaussian_datapoints], axis=1))
         self.datapoints = np.concatenate(self.datapoints)
         np.random.shuffle(self.datapoints)
--- test/ArtificialDatasets/test_gaussian_dataset.py
+++ test/ArtificialDatasets/test_gaussian_dataset.py
@@ -2,17 +2,23 @@ import numpy as np
 
 from src.ArtificialDatasets.gaussian_dataset import GaussianDataset
 
-def test_single_cluster():
-    dataset = GaussianDataset([([1, 1], [[1, 0], [0, 1]])], 1000)
+def test_single_class():
+    dataset = GaussianDataset([[([1, 1], [[1, 0], [0, 1]])]], 1000)
     assert dataset.num_datapoints == 1000
     assert dataset.relative_probs == [1.0]
     assert sum(dataset.datapoints[:, 0] == np.zeros(1000)) == 1000
 
-def test_2_cluster():
-    dataset = GaussianDataset([([1, 1], [[1, 0], [0, 1]]), ([-1, 1], [[1, 0], [0, 1]])], 1000, [0.5, 0.5])
+def test_2_class():
+    dataset = GaussianDataset([[([1, 1], [[1, 0], [0, 1]])], [([-1, 1], [[1, 0], [0, 1]])]], 1000, [0.5, 0.5])
     assert dataset.num_datapoints == 1000
     assert dataset.relative_probs == [0.5, 0.5]
     num_zero_index = sum(dataset.datapoints[:, 0] == np.zeros(1000))
     num_one_index = sum(dataset.datapoints[:, 0] == np.ones(1000))
     assert abs(num_zero_index - num_one_index) <= 200
     assert num_zero_index + num_one_index == 1000
+
+def test_multi_cluster_classes():
+    class_0_clusters = [([1, 1], [[1, 0], [0, 1]]), ([-1, -1], [[1, 0], [0, 1]])]
+    class_1_clusters = [([-1, 1], [[1, 0], [0, 1]]), ([1, -1], [[1, 0], [0, 1]])]
+    dataset = GaussianDataset([class_0_clusters, class_1_clusters], 10000)
+    assert dataset.num_datapoints == 10000
