b49a52f10077db999d20290172a6555b5cfa2bbe
william sweetenham <william@sweetenham.org>
2022-05-02 14:56:45 +0100

    Added backward function to tensor class and tests

--- src/RGrad/tensor.py
+++ src/RGrad/tensor.py
@@ -12,9 +12,10 @@ class Tensor:
         self.function = function
         self.shape = array.shape
         self.tensor_index = tensor_index
+        self.grad_array = None
         tensor_index += 1
 
-    def get_meta_graph():
+    def get_meta_graph(self):
         meta_graph = {}
         meta_graph_nodes = {}
 
@@ -25,8 +26,9 @@ class Tensor:
             for tensor_index, child_node in child_nodes.items():
                 meta_graph[tensor_index] = 0
                 meta_graph_nodes[tensor_index] = child_node
-                for parent_node in child_node.parents:
-                    parent_nodes[parent_node.index] = parent_node
+                if child_node.parents is not None:
+                    for parent_node in child_node.parents:
+                        parent_nodes[parent_node.tensor_index] = parent_node
             if len(parent_nodes) == 0:
                 break
             child_nodes = parent_nodes
@@ -36,12 +38,46 @@ class Tensor:
             raise RuntimeError('all 10000 iterations used in building meta-graph')
 
         for tensor_index, tensor in meta_graph_nodes.items():
-            for parent_tensor in tensor.parents:
-                meta_graph[parent_tensor.tensor_index] += 1
+            if tensor.parents is not None:
+                for parent_tensor in tensor.parents:
+                    meta_graph[parent_tensor.tensor_index] += 1
         
         return meta_graph
+    
+    def add_grad_contribution(self, index):
+        parent_tensor = self.parents[index]
+        derriv_array = np.zeros(parent_tensor.shape)
+        backward_tensor = self.function.backward(*self.parents, index)
+        for index in np.ndindex(self.shape):
+            derriv_array += self.grad_array[index]*backward_tensor[index]
+        if parent_tensor.grad_array is None:
+            parent_tensor.grad_array = derriv_array
+        else:
+            parent_tensor.grad_array += derriv_array
     
     def backward(self):
         if self.shape != ():
             raise ValueError('Tensor is not a scalar')
+        self.grad_array = np.array(1)
         meta_graph = self.get_meta_graph()
+        num_child_grads_added = {}
+        gradients_known = {self.tensor_index: self}
+        gradients_added = {}
+        while len(gradients_known) + len(gradients_added) < len(meta_graph):
+            new_gradients_known = {}
+            for tensor_index, tensor in gradients_known.items():
+                if tensor.parents is not None:
+                    for n in range(len(tensor.parents)):
+                        tensor.add_grad_contribution(n)
+                        parent_tensor_index = tensor.parents[n].tensor_index
+                        if parent_tensor_index in num_child_grads_added:
+                            num_child_grads_added[parent_tensor_index] += 1
+                        else:
+                            num_child_grads_added[parent_tensor_index] = 1
+                        if num_child_grads_added[parent_tensor_index] == meta_graph[parent_tensor_index]:
+                            new_gradients_known[parent_tensor_index] = tensor.parents[n]
+                gradients_added[tensor_index] = tensor
+            gradients_known = new_gradients_known
+
+
+                        
--- test/RGrad/test_tensor.py
+++ test/RGrad/test_tensor.py
@@ -1,8 +1,56 @@
 import numpy as np
 
 from src.RGrad.tensor import Tensor
+from src.RGrad.function import matmul, relu, mean
+
 
 def test_tensor():
     tensor1 = Tensor(np.array([1, 2]))
     tensor2 = Tensor(np.array([1, 2]))
     assert tensor2.tensor_index == tensor1.tensor_index + 1
+
+
+def test_meta_graph():
+    tensor1 = Tensor(np.array([[1, 2], [3, 4]]))
+    tensor2 = Tensor(np.array([[5, 6], [7, 8]]))
+    tensor3 = matmul(tensor1, tensor2)
+    tensor4 = relu(tensor2)
+    tensor5 = matmul(tensor3, tensor4)
+    tensor6 = matmul(tensor5, tensor4)
+    tensor7 = relu(tensor5)
+    tensor8 = matmul(tensor5, tensor6)
+    tensor9 = mean(tensor8)
+    meta_graph = tensor9.get_meta_graph()
+    assert meta_graph == {tensor1.tensor_index: 1,
+                          tensor2.tensor_index: 2,
+                          tensor3.tensor_index: 1,
+                          tensor4.tensor_index: 2,
+                          tensor5.tensor_index: 2,
+                          tensor6.tensor_index: 1,
+                          tensor8.tensor_index: 1,
+                          tensor9.tensor_index: 0}
+
+def test_add_grad_contribution():
+    tensor3 = Tensor(np.array([[1, 2], [3, 4]]))
+    tensor4 = Tensor(np.array([[5, 6], [7, 8]]))
+    tensor2 = matmul(tensor3, tensor4)
+    tensor1 = mean(tensor2)
+    tensor1.grad_array = np.array(1)
+    tensor1.add_grad_contribution(0)
+    assert np.allclose(tensor2.grad_array, np.array([[0.25, 0.25], [0.25, 0.25]]))
+    tensor2.add_grad_contribution(0)
+    assert np.allclose(tensor3.grad_array, np.array([[2.75, 3.75], [2.75, 3.75]]))
+
+def test_backward():
+    tensor1 = Tensor(np.array([[1, 2], [3, 4]]))
+    tensor2 = Tensor(np.array([[5, 6], [7, 8]]))
+    tensor3 = matmul(tensor1, tensor2)
+    tensor4 = mean(tensor1)
+    tensor5 = mean(tensor3)
+    tensor5.backward()
+    assert tensor4.grad_array is None
+    assert np.allclose(tensor5.grad_array, np.array(1))
+    assert np.allclose(tensor3.grad_array, np.array([[0.25, 0.25], [0.25, 0.25]]))
+    assert np.allclose(tensor1.grad_array, np.array([[2.75, 3.75], [2.75, 3.75]]))
+    assert np.allclose(tensor2.grad_array, np.array([[1, 1], [1.5, 1.5]]))
+    assert tensor2.grad_array.shape == (2, 2)
