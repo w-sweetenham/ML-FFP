1c96604fa433761a59a29c4fbf72ac8c8e38ffca
william sweetenham <william@sweetenham.org>
2022-05-29 15:53:10 +0100

    Updated test dataset and added dataloader class and tests

--- src/datahandling/dataset.py
+++ src/datahandling/dataset.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from src.RGrad.tensor import Tensor
+
 
 class LabelledDataset:
 
@@ -26,7 +28,44 @@ class CSVImageDataset(LabelledDataset):
             label = int(split_line[0])
             if len(pixel_vals) != num_pixels:
                 raise ValueError('invalid line length')
-            image_array = np.array(image_dims)
+            image_array = np.zeros(image_dims)
             for row_num in range(image_dims[0]):
                 image_array[row_num] = pixel_vals[row_num*image_dims[1]:row_num*image_dims[1] + image_dims[1]]
             self.datapoints.append((image_array, label))
+
+
+class DataLoader:
+
+    def __init__(self, dataset, batch_size, shuffle=False):
+        self.dataset = dataset
+        self.shuffle = shuffle
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        num_datapoints = self.dataset.num_datapoints()
+        self.index_sequence = np.arange(num_datapoints)
+        if self.shuffle:
+            np.random.shuffle(self.index_sequence)
+        num_full_batches = num_datapoints // self.batch_size
+        self.index_sequence_tuples = []
+        for batch_num in range(num_full_batches):
+            self.index_sequence_tuples.append((batch_num*self.batch_size,(batch_num*self.batch_size) + self.batch_size))
+        if num_full_batches*self.batch_size < num_datapoints:
+            self.index_sequence_tuples.append((num_full_batches*self.batch_size, num_datapoints))
+
+        self.index_tuples_index = 0
+        return self
+
+    def __next__(self):
+        if self.index_tuples_index < len(self.index_sequence_tuples):
+            datapoint_indices = self.index_sequence[self.index_sequence_tuples[self.index_tuples_index][0]:self.index_sequence_tuples[self.index_tuples_index][1]]
+            self.index_tuples_index += 1
+            image_list = []
+            label_list = []
+            for index in datapoint_indices:
+                image, label = self.dataset.get_datapoint(index)
+                image_list.append(image)
+                label_list.append(label)
+            return Tensor(np.stack(image_list)), Tensor(np.array(label_list))
+        else:
+            raise StopIteration
--- test/datahandling/test_dataset.py
+++ test/datahandling/test_dataset.py
@@ -1,2 +1,45 @@
+import os
+import numpy as np
+
+from src.datahandling.dataset import CSVImageDataset, DataLoader
+
+
 def test_csv_image_dataset():
-    pass
+    dataset = CSVImageDataset(os.path.join('datasets', 'test_image_dataset.csv'), (2, 3))
+    im1_correct = np.array([[0, 5/255, 1], [253/255, 0, 7/255]])
+    label1_correct = 0
+    im2_correct = np.array([[254/255, 1, 0], [0, 1/255, 0]])
+    label2_correct = 7
+
+    im1, label1 = dataset.get_datapoint(0)
+    im2, label2 = dataset.get_datapoint(1)
+
+    assert np.allclose(im1, im1_correct)
+    assert label1 == label1_correct
+    assert np.allclose(im2, im2_correct)
+    assert label2 == label2_correct
+
+
+def test_dataloader():
+    dataset = CSVImageDataset(os.path.join('datasets', 'test_image_dataset.csv'), (2, 3))
+    dataloader = DataLoader(dataset, 2)
+    image_batches = []
+    label_batches = []
+    for image_batch, label_batch in dataloader:
+        image_batches.append(image_batch)
+        label_batches.append(label_batch)
+    
+    image_batch1_correct = np.array([[[0, 5/255, 1], [253/255, 0, 7/255]], [[254/255, 1, 0], [0, 1/255, 0]]])
+    label_batch1_correct = np.array([0, 7])
+    image_batch2_correct = np.array([[[8/255, 8/255, 254/255], [252/255, 4/255, 253/255]], [[251/255, 254/255, 1], [1, 1/255, 2/255]]])
+    label_batch2_correct = np.array([1, 9])
+    image_batch3_correct = np.array([[[250/255, 187/255, 100/255], [0, 0, 0]]])
+    label_batch3_correct = np.array([8])
+
+    assert np.allclose(image_batches[0].elems, image_batch1_correct)
+    assert np.all(label_batches[0].elems == label_batch1_correct)
+    assert np.allclose(image_batches[1].elems, image_batch2_correct)
+    assert np.all(label_batches[1].elems == label_batch2_correct)
+    assert np.allclose(image_batches[2].elems, image_batch3_correct)
+    assert np.all(label_batches[2].elems == label_batch3_correct)
+    
